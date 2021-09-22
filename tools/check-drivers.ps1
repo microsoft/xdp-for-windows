bff5e526fb86fe513d8ca1ddc1fb157be36e904f
Nick Banks <nibanks@microsoft.com>
2021-09-22 16:24:42 +0000

    Merged PR 17187: Check for XDP Drivers
    We've been seeing issues where it seems a previous version of one or more of the XDP drivers is still loaded on the system. This PR is an attempt to catch that by checking (before and after) if any known XDP driver is loaded when it shouldn't be.

---
+++ tools/check-drivers.ps1
@@ -0,0 +1,30 @@
+<#
+
+.SYNOPSIS
+This checks for the presence of any XDP drivers currently loaded.
+
+#>
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+# Cache driverquery output.
+$AllDrivers = driverquery /v
+
+# Checks for the presence of a loaded driver.
+function Check-Driver($Driver) {
+    $Found = $false
+    try {
+        $Found = ($AllDrivers | Select-String $Driver).Matches[0].Success
+    } catch { }
+    $Found
+}
+
+# Check for any XDP drivers.
+if (Check-Driver 'fndis.sys') { Write-Error "fndis.sys loaded!" }
+if (Check-Driver 'xdpfnmp.sys') { Write-Error "xdpfnmp.sys loaded!" }
+if (Check-Driver 'xdpmp.sys') { Write-Error "xdpmp.sys loaded!" }
+if (Check-Driver 'xdp.sys') { Write-Error "xdp.sys loaded!" }
+
+# Yay! No XDP drivers found.
+Write-Host "No loaded XDP drivers found!"
