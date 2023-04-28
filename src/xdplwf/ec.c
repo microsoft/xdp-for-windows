a377cb9e7f2e66d60c2193ccc6382d54458752e8
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2023-04-28 14:11:44 -0400

    ensure EC passive worker gets boosted during cleanup

--- src/xdplwf/ec.c
+++ src/xdplwf/ec.c
@@ -333,6 +333,12 @@ XdpEcCleanup(
         Ec->CleanupComplete = NULL;
 
         Ec->CleanupPassiveThread = TRUE;
+        //
+        // Rather than setting the priority boost while setting the event, which
+        // is effective only if the thread was not already READY, permanently
+        // boost the thread priority to expedite cleanup.
+        //
+        KeSetPriorityThread(Ec->PassiveThread, LOW_REALTIME_PRIORITY);
         KeSetEvent(&Ec->PassiveEvent, 0, FALSE);
         KeWaitForSingleObject(Ec->PassiveThread, Executive, KernelMode, FALSE, NULL);
         ObDereferenceObject(Ec->PassiveThread);
