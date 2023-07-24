ddaedcde9558d31d4423bafc522b5e5748670a65
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2023-07-24 15:29:36 -0400

    create test archive

---
+++ tools/create-test-archive.ps1
@@ -0,0 +1,49 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+# Creates an archive of XDP test artifacts to validate compatibility with newer
+# versions of XDP binaries.
+# Code must be built before running this script.
+#
+
+param (
+    [ValidateSet("x64")]
+    [Parameter(Mandatory=$false)]
+    [string]$Platform = "x64",
+
+    [ValidateSet("Debug", "Release")]
+    [Parameter(Mandatory=$false)]
+    [string]$Config = "Debug"
+)
+
+$RootDir = Split-Path $PSScriptRoot -Parent
+. $RootDir\tools\common.ps1
+
+$Name = "xdp-tests-$Platform"
+if ($Config -eq "Debug") {
+    $Name += "-debug"
+}
+$DstPath = "artifacts\tests\$Name"
+
+Remove-Item $DstPath -Recurse -ErrorAction Ignore
+New-Item -Path $DstPath -ItemType Directory > $null
+
+New-Item -Path $DstPath\bin -ItemType Directory > $null
+copy "artifacts\bin\$($Platform)_$($Config)\xdpfunctionaltests.dll" $DstPath\bin
+
+New-Item -Path $DstPath\symbols -ItemType Directory > $null
+copy "artifacts\bin\$($Platform)_$($Config)\xdpfunctionaltests.pdb"   $DstPath\symbols
+
+[xml]$XdpVersion = Get-Content $RootDir\src\xdp.props
+$Major = $XdpVersion.Project.PropertyGroup.XdpMajorVersion[0]
+$Minor = $XdpVersion.Project.PropertyGroup.XdpMinorVersion[0]
+$Patch = $XdpVersion.Project.PropertyGroup.XdpPatchVersion[0]
+
+$VersionString = "$Major.$Minor.$Patch"
+
+if (!((Get-BuildBranch).StartsWith("release/"))) {
+    $VersionString += "-prerelease+" + (git.exe describe --long --always --dirty --exclude=* --abbrev=8)
+}
+
+Compress-Archive -DestinationPath "$DstPath\$Name-$VersionString.zip" -Path $DstPath\*
