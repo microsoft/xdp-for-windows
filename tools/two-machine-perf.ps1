9e9e2397381d8288bc443e97e42ebf8cb9d9d321
Nick Banks <nibanks@microsoft.com>
2023-09-21 15:06:48 -0400

    Try fix logging

--- tools/two-machine-perf.ps1
+++ tools/two-machine-perf.ps1
@@ -11,7 +11,10 @@ param (
     [string]$PeerName = "netperf-peer",
 
     [Parameter(Mandatory = $false)]
-    [string]$RemotePSConfiguration = "PowerShell.7"
+    [string]$RemotePSConfiguration = "PowerShell.7",
+
+    [Parameter(Mandatory = $false)]
+    [string]$RemoteDir = "C:\_work"
 )
 
 #Set-StrictMode -Version 'Latest'
@@ -52,27 +55,29 @@ Write-Output "Remote interface: $RemoteInterface, $RemoteMacAddress"
 # Copy the artifacts to the peer.
 Write-Output "Copying files to peer..."
 Invoke-Command -Session $Session -ScriptBlock {
-    Remove-Item -Force -Recurse "C:\_work" -ErrorAction Ignore
-}
-Copy-Item -ToSession $Session .\artifacts -Destination C:\_work\artifacts -Recurse
-Copy-Item -ToSession $Session .\tools -Destination C:\_work\tools -Recurse
+    param ($RemoteDir)
+    Remove-Item -Force -Recurse $RemoteDir -ErrorAction Ignore
+} -ArgumentList $RemoteDir
+Copy-Item -ToSession $Session .\artifacts -Destination $RemoteDir\artifacts -Recurse
+Copy-Item -ToSession $Session .\tools -Destination $RemoteDir\tools -Recurse
 
 # Check for any previously drivers.
 Write-Output "Checking local machine state..."
 tools\check-drivers.ps1 -Config $Config -Arch $Arch -Verbose
 Write-Output "Checking remote machine state..."
 Invoke-Command -Session $Session -ScriptBlock {
-    param ($Config, $Arch)
-    C:\_work\tools\check-drivers.ps1 -Config $Config -Arch $Arch -Verbose
-} -ArgumentList $Config, $Arch
+    param ($Config, $Arch, $RemoteDir)
+    & $RemoteDir\tools\check-drivers.ps1 -Config $Config -Arch $Arch -Verbose
+} -ArgumentList $Config, $Arch, $RemoteDir
 
 # Prepare the machines for the testing.
 Write-Output "Preparing local machine for testing..."
 tools\prepare-machine.ps1 -ForNetPerfTest -NoReboot -Verbose
 Write-Output "Preparing remote machine for testing..."
 Invoke-Command -Session $Session -ScriptBlock {
-    C:\_work\tools\prepare-machine.ps1 -ForNetPerfTest -NoReboot -Verbose
-}
+    param ($RemoteDir)
+    & $RemoteDir\tools\prepare-machine.ps1 -ForNetPerfTest -NoReboot -Verbose
+} -ArgumentList $RemoteDir
 
 try {
 # Install XDP on the machines.
@@ -80,24 +85,28 @@ Write-Output "Installing XDP locally..."
 tools\setup.ps1 -Install xdp -Config $Config -Arch $Arch
 Write-Output "Installing XDP on peer..."
 Invoke-Command -Session $Session -ScriptBlock {
-    param ($Config, $Arch)
-    C:\_work\tools\setup.ps1 -Install xdp -Config $Config -Arch $Arch
-} -ArgumentList $Config, $Arch
+    param ($Config, $Arch, $RemoteDir)
+    & $RemoteDir\tools\setup.ps1 -Install xdp -Config $Config -Arch $Arch
+} -ArgumentList $Config, $Arch, $RemoteDir
 
 # Start logging.
+Write-Output "Starting local logs..."
+try { wpr.exe -cancel -instancename xskcpu 2>&1 } catch { }
 tools\log.ps1 -Start -Name xskcpu -Profile CpuSample.Verbose -Config $Config -Arch $Arch
+Write-Output "Starting remote logs..."
 Invoke-Command -Session $Session -ScriptBlock {
-    param ($Config, $Arch)
-    C:\_work\tools\log.ps1 -Start -Name xskcpu -Profile CpuSample.Verbose -Config $Config -Arch $Arch
-} -ArgumentList $Config, $Arch
+    param ($Config, $Arch, $RemoteDir)
+    try { wpr.exe -cancel -instancename xskcpu 2>&1 } catch { }
+    & $RemoteDir\tools\log.ps1 -Start -Name xskcpu -Profile CpuSample.Verbose -Config $Config -Arch $Arch
+} -ArgumentList $Config, $Arch, $RemoteDir
 
 # Run xskbench.
 Write-Output "Starting xskbench on the peer..."
 $Job = Invoke-Command -Session $Session -ScriptBlock {
-    param ($Config, $Arch, $LocalInterface)
-    $XskBench = "C:\_work\artifacts\bin\$($Arch)_$($Config)\xskbench.exe"
+    param ($Config, $Arch, $RemoteDir, $LocalInterface)
+    $XskBench = "$RemoteDir\artifacts\bin\$($Arch)_$($Config)\xskbench.exe"
     & $XskBench rx -i $LocalInterface -d 15 -t -q -id 0
-} -ArgumentList $Config, $Arch, $RemoteInterface -AsJob
+} -ArgumentList $Config, $Arch, $RemoteDir, $RemoteInterface -AsJob
 
 Write-Output "Running xskbench locally..."
 $XskBench = "artifacts\bin\$($Arch)_$($Config)\xskbench.exe"
@@ -113,19 +122,19 @@ Write-Output "Test Complete"
     if (Test-Path logs) {
         Write-Output "Stopping remote logs..."
         Invoke-Command -Session $Session -ScriptBlock {
-            param ($Config, $Arch)
-            C:\_work\tools\log.ps1 -Stop -Name xskcpu -Config $Config -Arch $Arch -EtlPath C:\_work\artifacts\logs\xskbench-peer.etl
-        } -ArgumentList $Config, $Arch
+            param ($Config, $Arch, $RemoteDir)
+            & $RemoteDir\tools\log.ps1 -Stop -Name xskcpu -Config $Config -Arch $Arch -EtlPath $RemoteDir\artifacts\logs\xskbench-peer.etl
+        } -ArgumentList $Config, $Arch, $RemoteDir
         Write-Output "Stopping local logs..."
         tools\log.ps1 -Stop -Name xskcpu -Config $Config -Arch $Arch -EtlPath artifacts\logs\xskbench-local.etl
         Write-Output "Copying remote logs..."
-        Copy-Item -FromSession $Session C:\_work\artifacts\logs\xskbench-peer.etl -Destination artifacts\logs
+        Copy-Item -FromSession $Session $RemoteDir\artifacts\logs\xskbench-peer.etl -Destination artifacts\logs
     }
     Write-Output "Removing XDP locally..."
     tools\setup.ps1 -Uninstall xdp -Config $Config -Arch $Arch -ErrorAction 'Continue'
     Write-Output "Removing XDP on peer..."
     Invoke-Command -Session $Session -ScriptBlock {
-        param ($Config, $Arch)
-        C:\_work\tools\setup.ps1 -Uninstall xdp -Config $Config -Arch $Arch -ErrorAction 'Continue'
-    } -ArgumentList $Config, $Arch
+        param ($Config, $Arch, $RemoteDir)
+        & $RemoteDir\tools\setup.ps1 -Uninstall xdp -Config $Config -Arch $Arch -ErrorAction 'Continue'
+    } -ArgumentList $Config, $Arch, $RemoteDir
 }
