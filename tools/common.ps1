c87441d76bb1c311e93b2451f264574a9dd7f616
Alan Jowett <alan.jowett@microsoft.com>
2023-10-27 15:44:32 -0700

    Switch to ebpf_for_windows nuget version 0.12.0
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- tools/common.ps1
+++ tools/common.ps1
@@ -110,7 +110,7 @@ function Get-EbpfInstallPath {
 
 # Returns the eBPF MSI filename
 function Get-EbpfMsiFilename {
-    return "ebpf-for-windows.0.9.0+5122375852.msi"
+    return "ebpf-for-windows.0.12.0+5122375852.msi"
 }
 
 # Returns the eBPF MSI full path
