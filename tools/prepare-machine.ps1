11c1c7cb41009fbcdbbfb9660ab9179b6e057b75
Nick Banks <nibanks@microsoft.com>
2024-01-19 12:10:26 -0500

    Just redirect error output

--- tools/prepare-machine.ps1
+++ tools/prepare-machine.ps1
@@ -196,9 +196,7 @@ function Install-Certs {
     if (!(Test-Path $CodeSignCertPath)) {
         Write-Error "$CodeSignCertPath does not exist!"
     }
-    Write-Host "CertUtil.exe -f -addstore Root $CodeSignCertPath"
     CertUtil.exe -f -addstore Root $CodeSignCertPath 2>&1 | Write-Verbose
-    Write-Host "CertUtil.exe -f -addstore trustedpublisher $CodeSignCertPath"
     CertUtil.exe -f -addstore trustedpublisher $CodeSignCertPath 2>&1 | Write-Verbose
 }
 
@@ -346,17 +344,11 @@ if ($Cleanup) {
     }
 
     if ($ForNetPerfTest) {
-        Write-Host "Setup-VcRuntime"
         Setup-VcRuntime
-        Write-Host "Download-CoreNet-Deps"
         Download-CoreNet-Deps
-        Write-Host "Download-Ebpf-Msi"
         Download-Ebpf-Msi
-        Write-Host "Setup-TestSigning"
         Setup-TestSigning
-        Write-Host "Install-Certs"
         Install-Certs
-        Write-Host "Done"
     }
 
     if ($ForTest) {
