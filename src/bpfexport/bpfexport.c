04a41ca848f8b8e3d00300308382ebf65c4f9995
Shankar Seal <sseal@microsoft.com>
2024-01-21 21:59:38 -0800

    wip

---
+++ src/bpfexport/bpfexport.c
@@ -0,0 +1,57 @@
+//
+// Copyright (c) Microsoft Corporation.
+//
+
+#include <stdio.h>
+#include <windows.h>
+//
+// Work around various eBPF compilation bugs.
+//
+#define USER_MODE 1
+#pragma warning(push)
+#pragma warning(disable:4005) // 'WIN32_LEAN_AND_MEAN': macro redefinition
+#include <ebpf_store_helper.h>
+#include <ebpfstore.h>
+#pragma warning(pop)
+#undef USER_MODE
+
+INT
+__cdecl
+main(
+    INT argc,
+    CHAR *argv[]
+    )
+{
+    ebpf_result_t result;
+    int exit_code = 0;
+
+    if (argc == 2 && !_strcmpi("--clear", argv[1])) {
+        for (uint32_t i = 0; i < sizeof(EbpfXdpSectionInfo) / sizeof(EbpfXdpSectionInfo[0]); i++) {
+            result = ebpf_store_delete_section_information(&EbpfXdpSectionInfo[i]);
+            if (result != EBPF_SUCCESS) {
+                fprintf(stderr, "ebpf_store_delete_section_information failed: %u\n", result);
+                exit_code = -1;
+            }
+        }
+
+        result = ebpf_store_delete_program_information(&EbpfXdpProgramInfo);
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "ebpf_store_delete_program_information failed: %u\n", result);
+            exit_code = -1;
+        }
+    } else {
+        result = ebpf_store_update_section_information(&EbpfXdpSectionInfo[0], RTL_NUMBER_OF(EbpfXdpSectionInfo));
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "ebpf_store_update_section_information failed: %u\n", result);
+            exit_code = -1;
+        }
+
+        result = ebpf_store_update_program_information(&EbpfXdpProgramInfo, 1);
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "ebpf_store_update_program_information failed: %u\n", result);
+            exit_code = -1;
+        }
+    }
+
+    return exit_code;
+}
---
+++ src/xdp/ebpfstore.h
@@ -0,0 +1,58 @@
+//
+// Copyright (c) Microsoft Corporation.
+//
+
+#pragma once
+
+#include <ebpf_extension.h>
+#include <ebpf_extension_uuids.h>
+#include <ebpf_nethooks.h>
+#include <ebpf_program_attach_type_guids.h>
+#include <ebpf_program_types.h>
+#include <ebpf_structs.h>
+#include <ebpf_private_extension.h>
+
+static const ebpf_context_descriptor_t EbpfXdpContextDescriptor = {
+    .size = sizeof(xdp_md_t),
+    .data = FIELD_OFFSET(xdp_md_t, data),
+    .end = FIELD_OFFSET(xdp_md_t, data_end),
+    .meta = FIELD_OFFSET(xdp_md_t, data_meta),
+};
+
+#define XDP_EXT_HELPER_FUNCTION_START EBPF_MAX_GENERAL_HELPER_FUNCTION
+
+// XDP helper function prototype descriptors.
+static const ebpf_helper_function_prototype_t EbpfXdpHelperFunctionPrototype[] = {
+    {
+        .helper_id = XDP_EXT_HELPER_FUNCTION_START + 1,
+        .name = "bpf_xdp_adjust_head",
+        .return_type = EBPF_RETURN_TYPE_INTEGER,
+        .arguments = {
+            EBPF_ARGUMENT_TYPE_PTR_TO_CTX,
+            EBPF_ARGUMENT_TYPE_ANYTHING,
+        },
+    },
+};
+
+#pragma warning(suppress:4090) // 'initializing': different 'const' qualifiers
+const ebpf_program_info_t EbpfXdpProgramInfo = {
+#pragma warning(suppress:4090) // 'initializing': different 'const' qualifiers
+    .program_type_descriptor = {
+        .name = "xdp",
+        .context_descriptor = &EbpfXdpContextDescriptor,
+        .program_type = EBPF_PROGRAM_TYPE_XDP_INIT,
+        BPF_PROG_TYPE_XDP,
+    },
+    .count_of_program_type_specific_helpers = RTL_NUMBER_OF(EbpfXdpHelperFunctionPrototype),
+    .program_type_specific_helper_prototype = EbpfXdpHelperFunctionPrototype,
+};
+
+#define DECLARE_XDP_SECTION(_section_name) \
+    (const wchar_t*)_section_name, &EBPF_PROGRAM_TYPE_XDP, &EBPF_ATTACH_TYPE_XDP, \
+    BPF_PROG_TYPE_XDP, BPF_XDP
+
+const ebpf_program_section_info_t DECLSPEC_SELECTANY EbpfXdpSectionInfo[] = {
+    {
+        DECLARE_XDP_SECTION(L"xdp")
+    }
+};
