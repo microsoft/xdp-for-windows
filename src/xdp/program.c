d28d6207e2ff34b3b9937cb5062ce17fc0af8294
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-06-26 14:10:52 -0400

    fix ebpf struct version validation

--- src/xdp/program.c
+++ src/xdp/program.c
@@ -1633,7 +1633,10 @@ EbpfProgramOnClientAttach(
     TraceEnter(
         TRACE_CORE, "AttachingProvider=%p AttachingClient=%p", AttachingProvider, AttachingClient);
 
-    if (ClientData == NULL || ClientData->header.size != sizeof(IfIndex) || ClientData->data == NULL) {
+    if (ClientData == NULL ||
+        ClientData->header.version != 1 ||
+        ClientData->header.size != sizeof(IfIndex) ||
+        ClientData->data == NULL) {
         Status = STATUS_INVALID_PARAMETER;
         goto Exit;
     }
