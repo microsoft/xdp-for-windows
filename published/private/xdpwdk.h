0d6f361b0ef1fd0544a3c501d6167cfc2706e1c1
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-06-28 11:36:34 -0400

    try again: vs2019

--- published/private/xdpwdk.h
+++ published/private/xdpwdk.h
@@ -12,7 +12,7 @@
 
 #if NDIS_SUPPORT_NDIS630
 
-#if (!defined(NTDDI_WIN10_CO) || (WDK_NTDDI_VERSION < NTDDI_WIN10_CO))
+#if (!defined(NTDDI_WIN10_NI) || (WDK_NTDDI_VERSION < NTDDI_WIN10_NI))
 
 inline
 BOOLEAN
@@ -25,6 +25,6 @@ NET_BUFFER_LIST_IS_TCP_RSC_SET(
     return ((UINT_PTR)Info->Value & 0xFFFFFFFF) != 0;
 }
 
-#endif // (!defined(NTDDI_WIN10_CO) || (WDK_NTDDI_VERSION < NTDDI_WIN10_CO))
+#endif // (!defined(NTDDI_WIN10_NI) || (WDK_NTDDI_VERSION < NTDDI_WIN10_NI))
 
 #endif // NDIS_SUPPORT_NDIS630
