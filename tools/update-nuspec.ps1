32fc529d09ad8d18aa8c285c85641cac1e146981
Michael Friesen (NETWORKING) <3517159+mtfriesen@users.noreply.github.com>
2024-07-31 13:47:40 -0400

    append prerelease string to nuget version outside of release builds

--- tools/update-nuspec.ps1
+++ tools/update-nuspec.ps1
@@ -10,10 +10,10 @@ $RootDir = Split-Path $PSScriptRoot -Parent
 . $RootDir\tools\common.ps1
 
 $Commit = git.exe rev-parse HEAD
-$Ver = Get-XdpBuildVersion
+$VersionString = Get-XdpBuildVersionString
 $content = Get-Content $InputFile
 $content = $content.Replace("{commit}", $Commit)
-$content = $content.Replace("{version}", "$($Ver.Major).$($Ver.Minor).$($Ver.Patch)")
+$content = $content.Replace("{version}", $VersionString)
 $content = $content.Replace("{rootpath}", $RootDir)
 $content = $content.Replace("{binpath}", $(Get-ArtifactBinPath -Arch $Arch -Config $Config))
 set-content $OutputFile $content
