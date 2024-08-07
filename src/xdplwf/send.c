f13550fbffc2c71a31774acec05c869e1a40eb4e
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-08-07 14:08:21 -0400

    fix build

--- src/xdplwf/send.c
+++ src/xdplwf/send.c
@@ -796,7 +796,7 @@ XdpGenericTxCreateQueue(
     TxCapabilities.OutOfOrderCompletionEnabled = TRUE;
     TxCapabilities.MaximumBufferSize = MAX_TX_BUFFER_LENGTH;
     TxCapabilities.MaximumFrameSize = Generic->Tx.Mtu;
-    TxCapabilities.TransmitFrameCountHint = TxQueue->FrameCount;
+    TxCapabilities.TransmitFrameCountHint = (UINT16)min(MAXUINT16, TxQueue->FrameCount);
     XdpTxQueueSetCapabilities(Config, &TxCapabilities);
 
     *InterfaceTxQueue = (XDP_INTERFACE_HANDLE)TxQueue;
