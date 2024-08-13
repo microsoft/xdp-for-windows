b9d57365ad12940f12b796735798f5f9bcdbc4c3
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-08-13 12:08:35 -0400

    create runtime nuget (#638)
    * WIP
    
    * add xdp runtime nuget
    
    * avoid ..\..

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -69,7 +69,7 @@ $XdpFileVersion = (Get-Item "$ArtifactsDir\xdp\xdp.sys").VersionInfo.FileVersion
 # Determine the XDP build version string from xdp.sys. The Windows file version
 # format is "A.B.C.D", but XDP (and semver) use only the "A.B.C".
 $XdpFileVersion = $XdpFileVersion.substring(0, $XdpFileVersion.LastIndexOf('.'))
-$XdpMsiFullPath = "$ArtifactsDir\xdpinstaller\xdp-for-windows.$XdpFileVersion.msi"
+$XdpMsiFullPath = "$ArtifactsDir\xdp-for-windows.$XdpFileVersion.msi"
 $FndisSys = "$ArtifactsDir\test\fndis\fndis.sys"
 $XdpMpSys = "$ArtifactsDir\test\xdpmp\xdpmp.sys"
 $XdpMpInf = "$ArtifactsDir\test\xdpmp\xdpmp.inf"
