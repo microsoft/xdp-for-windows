b49cb5ef46cb7534e8dd295d32e9121bc462635b
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-08-15 11:10:46 -0400

    install signing cert to publishers

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -180,7 +180,7 @@ function Install-DriverCertificate($CertFileName) {
     $Chain.ChainElements.Certificate | Select-Object -Last 1 | Export-Certificate -Type CERT -FilePath $CertRootFileName | Write-Verbose
 
     Import-Certificate -FilePath $CertRootFileName -CertStoreLocation 'cert:\localmachine\root' | Write-Verbose
-    Import-Certificate -FilePath $CertRootFileName -CertStoreLocation 'cert:\localmachine\trustedpublisher' | Write-Verbose
+    Import-Certificate -FilePath $CertFileName -CertStoreLocation 'cert:\localmachine\trustedpublisher' | Write-Verbose
 }
 
 function Install-SignedDriverCertificate($SignedFileName) {
