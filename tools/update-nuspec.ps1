21fed955256b55bd1ab63ffe1ed8ce5a38d9c7df
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-09-27 16:36:01 -0400

    recant and make runtime nugets per-architecture (#694)

--- tools/update-nuspec.ps1
+++ tools/update-nuspec.ps1
@@ -15,6 +15,7 @@ $content = Get-Content $InputFile
 $content = $content.Replace("{commit}", $Commit)
 $content = $content.Replace("{version}", $VersionString)
 $content = $content.Replace("{rootpath}", $RootDir)
+$content = $content.Replace("{arch}", $Platform)
 $content = $content.Replace("{anyarch}", $Platform)
 $content = $content.Replace("{binpath_anyarch}", $(Get-ArtifactBinPath -Platform $Platform -Config $Config))
 set-content $OutputFile $content
