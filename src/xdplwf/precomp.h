6bbaf58c9633504b4c8482d358713beb01bdc6a5
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-09-27 13:50:20 -0400

    make xdp build again on latest wdk (#693)
    * make xdp build again on latest wdk
    
    * use ws2019 version consistently as minimum
    
    * fix build?
    
    * more
    
    * gah!

--- src/xdplwf/precomp.h
+++ src/xdplwf/precomp.h
@@ -61,8 +61,6 @@
 
 #ifndef NDIS_RUNTIME_VERSION_688
 #define NDIS_RUNTIME_VERSION_688 ((6 << 16) | 88)
-#else
-C_ASSERT(!"NDIS_RUNTIME_VERSION_688 has been publicly defined. Remove redundant definition.");
 #endif
 
 #pragma warning(disable:4200) // nonstandard extension used: zero-sized array in struct/union
