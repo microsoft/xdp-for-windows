58b50c6695c2629f09318ace2bcab04bf49faca4
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2024-10-21 15:37:45 -0400

    force compiler to read on debug

--- src/xdp/dispatch.c
+++ src/xdp/dispatch.c
@@ -524,10 +524,10 @@ DriverEntry(
 
     TraceEnter(TRACE_CORE, "DriverObject=%p", DriverObject);
 
-    LONG64 i64;
+    LONG64 i64 = 0;
     LONG *i32 = RTL_PTR_ADD(&i64, 1); // this is an intentionally misaligned 32-bit integer.
 #if DBG
-    ReadNoFence(i32);
+    FRE_ASSERT(ReadNoFence(i32) == 0);
 #else
     InterlockedIncrement(i32);
 #endif
