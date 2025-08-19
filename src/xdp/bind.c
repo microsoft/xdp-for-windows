7af557af2694b5826e081d1a2f5c5d19968d1fb5
Jack He <jackhe@microsoft.com>
2025-08-18 19:29:52 -0700

    2 places where offloadobjects can be de-allocated.

--- src/xdp/bind.c
+++ src/xdp/bind.c
@@ -892,6 +892,18 @@ XdpIfCloseInterfaceOffloadHandle(
 {
     XDP_INTERFACE_SET *IfSet = (XDP_INTERFACE_SET *)IfSetHandle;
     XDP_IF_OFFLOAD_OBJECT *OffloadObject = (XDP_IF_OFFLOAD_OBJECT *)InterfaceOffloadHandle;
+
+    // !!!TODO
+    if (IfSet == NULL) {
+        //
+        // The interface was deleted before we could close the offload handle.
+        // Assume proper cleanup has been performed in the delete code paths.
+        //
+        XdpIfpDereferenceInterfaceOffloadObject(OffloadObject);
+        return;
+    }
+
+
     BOOLEAN NeedCleanup = FALSE;
 
     TraceEnter(
