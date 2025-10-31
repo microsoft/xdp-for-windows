45f40bcb75ea138d9712d1d8faba8fb4a881238a
Jack He <jackhe@microsoft.com>
2025-10-31 12:22:52 -0700

    add rule

--- test/spinxsk/spinxsk.c
+++ test/spinxsk/spinxsk.c
@@ -870,7 +870,7 @@ AttachXdpProgram(
         rule.Match = XDP_MATCH_ALL;
 
         if (!(RandUlong() % 2)) {
-            rule.Match = RandUlong() % (XDP_MATCH_INNER_IPV6_DST_MASK_UDP + 1);
+            rule.Match = RandUlong() % (XDP_MATCH_ICMPV6_ECHO_REPLY_IP_DST + 1);
         }
 
         if (!(RandUlong() % 128)) {
